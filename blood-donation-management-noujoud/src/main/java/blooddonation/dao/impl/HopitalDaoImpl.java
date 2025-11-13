e407873ed4b0c4730ede9e877045d75450c42a7a
@sanaa-code01 <daoudsanae116@gmail.com>
2025-11-13 20:56:01 +0100

    Ajouter des methode à HopitalDaoImlp

--- blood-donation-management-noujoud/src/main/java/blooddonation/dao/impl/HopitalDaoImpl.java
+++ blood-donation-management-noujoud/src/main/java/blooddonation/dao/impl/HopitalDaoImpl.java
@@ -14,4 +14,66 @@ public class HopitalDaoImpl implements HopitalDao {
             return count.intValue();
         }
     }
+    @Override
+    public Hopital findById(int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Hopital.class, id);
+        }
+    }
+
+    @Override
+    public Hopital findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Hopital> query = session.createQuery("FROM Hopital WHERE email = :email", Hopital.class);
+            query.setParameter("email", email);
+            return query.uniqueResult();
+        }
+    }
+
+    @Override
+    public List<Hopital> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Hopital", Hopital.class).list();
+        }
+    }
+
+    @Override
+    public void save(Hopital hopital) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.persist(hopital);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(Hopital hopital) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.merge(hopital);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            Hopital hopital = session.get(Hopital.class, id);
+            if (hopital != null) session.remove(hopital);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
 }
